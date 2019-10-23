c36e51e4b406cbecd3070701049edaa8f5b93ed5
steve <steve@triple-corp.com>
2019-10-23 17:22:38 +0900

    emr commit

--- src/assets/js/ui.js
+++ src/assets/js/ui.js
@@ -12,6 +12,20 @@ EMDUI = (function () {
           loop:true,
           autoWidth:true,
         })
+
+        //TO-DO : 스크립트 개선
+        $('.gnb li').each(function(i){
+          $(this).mouseenter(function(){
+            $(this).addClass('actived');
+            $('.gnb-depth').show();
+          }).mouseleave(function(){
+            $(this).removeClass('actived');
+          });
+        });
+
+        $('.gnb-area').mouseleave(function(){
+          $('.gnb-depth').hide();
+        });
       }
     }
   };
